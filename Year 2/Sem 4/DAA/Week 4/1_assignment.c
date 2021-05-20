5e73a73f7bb4cee3113cbd1da248c67f955bf19d
Angad Sandhu <55819847+angadsinghsandhu@users.noreply.github.com>
2021-05-20 09:26:47 +0530

    additions

--- Year 2/Sem 4/DAA/Week 4/1_assignment.c
+++ Year 2/Sem 4/DAA/Week 4/1_assignment.c
@@ -2,83 +2,71 @@
 #include<stdlib.h>
 #include<limits.h>
 
-int min[1000], min_value = INT_MAX;
 
-void printCombinations(int size, int arr[size][size], int buf[size], int buf_1[size], int ix)
+int ans[1000], min = INT_MAX, opcount1 = 0, opcount2 = 0;
+
+void swap(int *a, int *b)
 {
-	int i, j;
-	
-	if(ix == size)
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void permute(int r, int arr[][r + 1], int per[], int l)
+{
+	int i;
+	if (l == r)
 	{
-		int sum = 0, flag = 1;
-		
-		for(i = 0; i < size; i++)
+		int sum = 0;
+		for (i = 0; i <= r; i++)
 		{
-			sum += buf[i];
+			opcount2++;
+			int idx = per[i];
+			sum += arr[i][idx];
 		}
-		
-		for(i = 0; i < size - 1; i++)
+		if (sum < min)
 		{
-			for(j = i + 1; j < size; j++)
+			for (i = 0; i <= r; i++)
 			{
-				if(buf_1[i] == buf_1[j])
-				{
-					flag = 0;
-					break;
-				}
+				int idx = per[i];
+				ans[i] = arr[i][per[i]];
 			}
-		}
-		
-		if(sum < min_value && flag)
-		{
-			for(i = 0; i < size; i++)
-			{
-				min[i] = buf[i];
-			}
-	
-			min_value = sum;
+			min = sum;
 		}
 	}
-	
 	else
 	{
-		for(i = 0; i < size; i++)
+		for (i = l; i <= r; i++)
 		{
-			buf[ix] = arr[i][ix];
-			buf_1[ix] = i;
-		
-			printCombinations(size, arr, buf, buf_1, ix + 1);
+			opcount1++;
+			swap((per + l), (per + i));
+			permute(r, arr, per, l + 1);
+			swap((per + l), (per + i));
 		}
 	}
 }
 
 int main()
 {
-	int i, j, size;
-	
-	printf("Enter the Size of the Square Matrix:\n");
-	scanf("%d", &size);
-	
-	int arr[size][size];
-	
-	printf("Enter Person vs Job Cost Matrix :\n");
-	
-	for(i = 0; i < size; i++)
+	int i, j, n;
+	printf("Enter the size of the square matrix : ");
+	scanf("%d", &n);
+	int arr[n][n];
+	printf("Enter the matrix : \n");
+	for (i = 0; i < n; i++)
 	{
-		for(j = 0; j < size; j++)
+		for (j = 0; j < n; j++)
 			scanf("%d", &arr[i][j]);
+			// arr[i][j]=1;
 	}
-
-	int buff[size];
-	int buff_1[size];
-	printCombinations(size, arr, buff, buff_1, 0);
-
-	printf("The Minimum Cost Combination is : ");
-
-	for(j = 0; j < size; j++)
-    	printf("%d ", min[j]);
-
-	printf("\nThe Minimum Cost is : %d\n", min_value);
-
+	int per[n];
+	for (i = 0; i < n; i++)
+		per[i] = i;
+	permute(n - 1, arr, per, 0);
+	printf("Combination for minimum cost : ");
+	for (i = 0; i < n; i++)
+		printf("%d ", ans[i]);
+	printf("\nThe Minimum Cost is : %d\n", min);
+	printf("Opcount = %d\n", opcount1 > opcount2 ? opcount1 : opcount2);
 	return 0;
 }
